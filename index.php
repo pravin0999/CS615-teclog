39086fcd76cf172e30b8fd0558404c33a2ebfff4
pravin0999 <pravinkumar@Pravins-MacBook-Pro.local>
2017-03-09 15:31:47 +0000

    edit

--- index.php
+++ index.php
@@ -16,9 +16,6 @@ if(isset($_COOKIE['ACTIVE_NOTE_ID'])) {
         $activeNoteId = $_COOKIE['ACTIVE_NOTE_ID'];
     }
 }
-echo " Hello World " ;
-echo " Hello World " ;
-echo " Hello World " ;
 
 
 if(isset($_REQUEST['action'])) {
@@ -53,5 +50,5 @@ $template->assign("notes", $db->getNotes());
 $template->display('index.tpl');
 
 //disconnect
-$db->disconnect();
+//$db->disconnect();
 ?>
