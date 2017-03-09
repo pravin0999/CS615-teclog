f4810fd8823a0a2d3a70b6808c0a8bd5f2f6e06f
pravin0999 <pravinkumar@Pravins-MacBook-Pro.local>
2017-03-09 23:44:39 +0000

    s

--- index.php
+++ index.php
@@ -28,12 +28,14 @@ if(isset($_REQUEST['action'])) {
             break;
         case 'update':
             $db->updateNote($_COOKIE['ACTIVE_NOTE_ID'], $_REQUEST['content']);
+            $content = $_REQUEST['content'];
+            echo $content;
             if(isset($_POST['email'])){
             $email = $_POST['email'];
             if(isset($_POST['flag'])){
             $flag = $_POST['flag'];
             echo $flag;
-            echo $_REQUEST['content'];
+            //echo $_REQUEST['content'];
             }
             echo $email;  
             }
