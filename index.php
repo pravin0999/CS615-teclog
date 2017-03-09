3faaed9acc8b48601f7596e21f40d9e11240d1c5
pravin0999 <pravinkumar@Pravins-MacBook-Pro.local>
2017-03-09 15:30:12 +0000

    Revert "edited"
    This reverts commit ad942518de1c8b844e27cb0582b4f45d871709bf.

--- index.php
+++ index.php
@@ -16,6 +16,9 @@ if(isset($_COOKIE['ACTIVE_NOTE_ID'])) {
         $activeNoteId = $_COOKIE['ACTIVE_NOTE_ID'];
     }
 }
+echo " Hello World " ;
+echo " Hello World " ;
+echo " Hello World " ;
 
 
 if(isset($_REQUEST['action'])) {
